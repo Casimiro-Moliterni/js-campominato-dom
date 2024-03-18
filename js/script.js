a444017a764023876dd8d776c4c76b2970953a38
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-18 16:14:56 +0100

    add : function array lenght

--- js/script.js
+++ js/script.js
@@ -1,53 +1,55 @@
-// Consegna
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-// Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-// Consigli del giorno:  :party_wizard:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Trovate allegato gli screenshot con e senza bonus e il logo da usare nell'header.
-
-// botton di avvio partita 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
 const mygrid = document.querySelector('.grid');
 const GenBtn = document.querySelector('.btn');
-GenBtn.addEventListener('click', general);
+GenBtn.addEventListener('click', generalGame);
 
 // funzione generale 
-function general(){
+function generalGame(){
+     //    svuotare griglia 
+     mygrid.innerHTML='';
 
-    
-    for (let i = 1; i <= 100; i++) {
+     const level = document.querySelector('#level').value;
+     let numberofBox ;
+     let numberoFCellsRow;
+     let numberRandom = [];
+     const bombs = generateRandomArray(16,1,16);
+     numberRandom.push(bombs);
+     console.log(numberRandom)
+     if(level === 'easy'){
+        numberofBox = 100;
+        numberoFCellsRow = 10 ;
+     } else if( level === 'hard'){
+        numberofBox = 81 ;
+        numberoFCellsRow = 9 ;
+     } else{
+        numberofBox = 49;
+        numberoFCellsRow = 7 ;
+     }
 
-
-        const thisElemnt = i ;
-        generateGridbox=thisElemnt;
+     console.log(numberofBox)
+    for (let i = 1; i <= numberofBox; i++) {
+        const thisElement = i ;
         
+        const box = generateGridbox(thisElement,numberoFCellsRow);
+        mygrid.append(box);    
     };
 }
    
 
-
-
 // Funzione che genera un quadrato
 // number -> numero che rappresenta un numero
 // return: elemento del dom che rappresenta un quadrato
-function generateGridbox(number) {
-
-
+function generateGridbox(number,numberoFCells) {
     const myDiv = document.createElement('div');
     myDiv.classList.add('box');
     myDiv.innerHTML = `<span>${number}</span>`;
-
+    myDiv.style.width= `calc(100% /${numberoFCells})`;
+    myDiv.style.height= `calc(100% /${numberoFCells})`;
     // funzione al click del box
     myDiv.addEventListener('click', function () {
         this.classList.toggle('color-skyblue');
@@ -56,4 +58,28 @@ function generateGridbox(number) {
     return myDiv;
 };
 
-// funzione ciclo di 100
+
+
+// funzione array number 
+function generateRandomArray(arrayLength, numMin, numMax) {
+    // Creiamo un array vuoto
+    const randomNumbersArray = [];
+
+    // finche non ci sono arrayLength numeri nell'array:
+    while(randomNumbersArray.length < arrayLength) {
+        // genero un numero random
+        const randNumber = getRndInteger(numMin, numMax);
+        // se il numero random non esiste nell'array lo pusho
+        if(!randomNumbersArray.includes(randNumber)) {
+            randomNumbersArray.push(randNumber);
+        }
+    }
+    
+    return randomNumbersArray;
+}
+
+// funzione numeri random
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
